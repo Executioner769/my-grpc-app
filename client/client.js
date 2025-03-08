ac4aa83dc178e800630d2900f5bb5b94f5214abd
Madhusudhan Reddy <madhuman1012@gmail.com>
2025-03-08 22:35:31 +0530

    implement greet many streaming api

--- client/client.js
+++ client/client.js
@@ -1,26 +1,68 @@
 const calculator = require("../server/protos/calculator_pb");
-const service = require("../server/protos/calculator_grpc_pb");
+const calculatorService = require("../server/protos/calculator_grpc_pb");
+
+const greet = require("../server/protos/greet_pb");
+const greetService = require("../server/protos/greet_grpc_pb");
 
 const grpc = require("@grpc/grpc-js");
 
-function main() {
-    const client = new service.CalculatorServiceClient(
+function callGreetManyTimes(firstName, lastName) {
+    const client = new greetService.GreetServiceClient(
+        "localhost:50051",
+        grpc.credentials.createInsecure()
+    );
+
+    const request = new greet.GreetManyTimesRequest();
+
+    const greeting = new greet.Greeting();
+    greeting.setFirstName(firstName);
+    greeting.setLastName(lastName);
+
+    request.setGreeting(greeting);
+
+    const call = client.greetManyTimes(request, () => {});
+
+    call.on("data", (response) => {
+        console.log("Client Streaming Response: ", response.getResult());
+    });
+
+    call.on("status", (status) => {
+        console.log(status);
+    });
+
+    call.on("error", (error) => {
+        console.error(error);
+    });
+
+    call.on("end", () => {
+        console.log("Streaming Ended!");
+    });
+}
+
+function callSum(num1, num2) {
+    const client = new calculatorService.CalculatorServiceClient(
         "localhost:50051",
         grpc.credentials.createInsecure()
     );
 
     const request = new calculator.SumRequest();
 
-    request.setFirstNumber(300);
-    request.setSecondNumber(1);
+    request.setFirstNumber(num1);
+    request.setSecondNumber(num2);
 
+    let result = 0;
     client.sum(request, (error, response) => {
         if (!error) {
             console.log(`Sum Result: ${response.getNumber()}`);
+            result = response.getNumber();
         } else {
             console.error(error);
         }
     });
+
+    return result;
 }
+
+function main() {}
 
 main();
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const calculator = require("./protos/calculator_pb");
-const service = require("./protos/calculator_grpc_pb");
+const calculatorService = require("./protos/calculator_grpc_pb");
+
+const greet = require("./protos/greet_pb");
+const greetService = require("./protos/greet_grpc_pb");
 
 const grpc = require("@grpc/grpc-js");
 
-/*
-    Implements the sum RPC method.
-*/
 function sum(call, callback) {
     const sumResponse = new calculator.SumResponse();
     sumResponse.setNumber(
@@ -14,9 +14,33 @@ function sum(call, callback) {
     callback(null, sumResponse);
 }
 
+function greetManyTimes(call, callback) {
+    const name =
+        call.request.getGreeting().getFirstName() +
+        " " +
+        call.request.getGreeting().getLastName();
+
+    let count = 0,
+        intervalID = setInterval(function () {
+            const greetManyTimesResponse = new greet.GreetManyTimesResponse();
+            greetManyTimesResponse.setResult(name);
+
+            // setup streaming
+            call.write(greetManyTimesResponse);
+
+            if (++count > 9) {
+                clearInterval(intervalID);
+                call.end(); // we have sent all messages!
+            }
+        }, 1000);
+}
+
 function main() {
     const server = new grpc.Server();
-    server.addService(service.CalculatorServiceService, { sum: sum });
+    server.addService(calculatorService.CalculatorServiceService, { sum: sum });
+    server.addService(greetService.GreetServiceService, {
+        greetManyTimes: greetManyTimes,
+    });
     server.bindAsync(
         "127.0.0.1:50051",
         grpc.ServerCredentials.createInsecure(),
